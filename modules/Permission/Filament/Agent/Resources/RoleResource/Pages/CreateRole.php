3d665795ce6a4ebb499d0a6764c712c802c846df
Hessam Taghvaei <mohaphez@yahoo.com>
2024-02-16 21:36:15 +0330

    feat: add Permission module for manage user permission and roles

---
+++ modules/Permission/Filament/Agent/Resources/RoleResource/Pages/CreateRole.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Permission\Filament\Agent\Resources\RoleResource\Pages;
+
+use Modules\Permission\Filament\Agent\Resources\RoleResource;
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+class CreateRole extends CreateRecord
+{
+    protected static string $resource = RoleResource::class;
+
+    public Collection $permissions;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->permissions = collect($data)
+            ->filter(fn ($permission, $key) => ! in_array($key, ['name', 'guard_name', 'select_all']))
+            ->values()
+            ->flatten();
+
+        return Arr::only($data, ['name', 'guard_name']);
+    }
+
+    protected function afterCreate(): void
+    {
+        $permissionModels = collect();
+        $this->permissions->each(function ($permission) use ($permissionModels): void {
+            $permissionModels->push(Utils::getPermissionModel()::firstOrCreate([
+                /** @phpstan-ignore-next-line */
+                'name'       => $permission,
+                'guard_name' => $this->data['guard_name'],
+            ]));
+        });
+
+        $this->record->syncPermissions($permissionModels);
+    }
+}
